ae555cc910567498f3eeb4a855d1305cb8c900d6
Ram <twinka4ik174@mail.ru>
2020-10-02 23:21:03 +0500

    project

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse
 from .models import Post
-from django.shortcuts import render
-from . import get_object_or_404
+from django.shortcuts import render, get_object_or_404
 
 
 def group_posts(request, slug):
