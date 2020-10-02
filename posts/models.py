46a9c6b1a0dbb01c08a4f9e017a37bfe2afac5db
Ram <twinka4ik174@mail.ru>
2020-10-02 19:17:49 +0500

    project

--- posts/models.py
+++ posts/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class Group(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
     descriptions = models.TextField()
     slug = models.SlugField()
     def __str__(self):
