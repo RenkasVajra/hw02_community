fd4a95ec03fd8ec9bb63e0673dec40ebdaae8587
Ram <twinka4ik174@mail.ru>
2020-10-02 19:38:39 +0500

    project

--- posts/models.py
+++ posts/models.py
@@ -6,8 +6,9 @@ User = get_user_model()
 
 class Group(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField()
     slug = models.SlugField()
+    description = models.TextField()
+    
     def __str__(self):
         return self.title
 
@@ -15,4 +16,5 @@ class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField("date published", auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
-    group = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts", blank=True, null=True)
+    group = models.ForeignKey(Group, on_delete=models.SET_NULL, 
+    related_name="posts", blank=True, null=True)
