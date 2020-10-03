3b73766cf69e16fdb8206317ba4d2aa54bf3fbac
Ram <twinka4ik174@mail.ru>
2020-10-03 12:19:44 +0500

    ss

--- migrations/0001_initial.py
+++ migrations/0001_initial.py
@@ -24,20 +24,4 @@ class Migration(migrations.Migration):
             ],
         ),
     ]
-    operations = [
-        migrations.CreateModel(
-            name='Group',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=100)),
-                ('descriptions', models.TextField()),
-                ('slug', models.SlugField()),
-            ],
-        ),
-        migrations.AddField(
-            model_name='post',
-            name='group',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
-        ),
-    ]
-
+    
